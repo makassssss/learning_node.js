bb7aa1ab8d57840ef806d24fe941095bebc549ad
Maxim Babichev <m.babichev@dinamicka.com>
2019-11-15 11:18:43 +0200

    chat model

---
+++ task9/server/src/migrations/20191112165958-create-chat-history.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('chatHistories', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            members: {
+                type: Sequelize.STRING
+            },
+            history: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('chatHistories');
+    }
+};
