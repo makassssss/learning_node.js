bfc3c82cdd486e442752be9a4f37d8b8f208d8b9
maks <mak.babichev@gmail.com>
2019-12-04 18:42:59 +0200

    chatHistory migration, seed file

--- task10/server/src/migrations/20191112165958-create-chat-history.js
+++ task10/server/src/migrations/20191112165958-create-chat-history.js
@@ -1,30 +1,31 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('chatHistories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      members: {
-        type: Sequelize.STRING
-      },
-      history: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('chatHistories');
-  }
+  up: (queryInterface, Sequelize) => (
+      queryInterface.createTable('chatHistory', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        from: {
+          type: Sequelize.STRING
+        },
+        to: {
+          type: Sequelize.STRING
+        },
+        text: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })
+  ),
+
+  down: queryInterface => queryInterface.dropTable('chatHistory'),
 };
