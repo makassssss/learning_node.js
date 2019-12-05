b160186a07bb5d4bf1405c2f006dc02e3858265d
maks <mak.babichev@gmail.com>
2019-12-05 15:01:01 +0200

    e2e: safari

--- task10/client/features/steps/auth.js
+++ task10/client/features/steps/auth.js
@@ -1,29 +1,32 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
-Given('I navigate to {string}', function (url) {
+Given('I navigate to {string}', (url) => {
+    browser.maximizeWindow();
     browser.url(url);
 });
 
-When('I enter my username {string} and password {string}', function (username, password) {
+When('I enter my username {string} and password {string}', (username, password) => {
     $('#username').setValue(username);
     $('#password').setValue(password);
 });
 
-When('I submit the form', function () {
+When('I submit the form', () => {
     $('input[type="submit"]').click();
 });
 
-Then('I should see the home page', function () {
-    assert.equal(browser.getUrl(), 'http://localhost:3000/departments')
+Then('I should see the home page', () => {
+    browser.waitUntil(() => {
+        return browser.getUrl() === 'http://localhost:3000/departments'
+    }, 1000, 'expected to see the home page');
 });
 
-Then('I should be able to logout', function () {
+Then('I should be able to logout', () => {
     $('#log-out').click();
     assert.equal(browser.getUrl(), 'http://localhost:3000/login')
 });
 
-Then('I should see an error', function () {
+Then('I should see an error', () => {
     assert.equal($('.validation').getText(), 'Invalid username or password!');
 });
 
