663a5026d6605076995dc3b43b45ed0bdb61a9c9
maks <mak.babichev@gmail.com>
2019-12-05 16:49:37 +0200

    fix jest tests

--- task10/client/jest.config.js
+++ task10/client/jest.config.js
@@ -76,7 +76,10 @@ module.exports = {
 	// ],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+	moduleNameMapper: {
+		"^containers(.*)$": "<rootDir>/src/containers$1",
+		"^components(.*)$": "<rootDir>/src/components$1",
+	},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
